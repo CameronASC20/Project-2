4651a944d18c5c15a14988e0b854de159d0a4473
CameronASC20 <camerongarnier73@gmail.com>
2022-03-16 10:32:00 -0400

    API works

---
+++ mongoose-express-liquid-boilerplate-main/models/superhero.js
@@ -0,0 +1,30 @@
+// import dependencies
+const mongoose = require('./connection')
+
+// import user model for populate
+const User = require('./user')
+
+// destructure the schema and model constructors from mongoose
+const { Schema, model } = mongoose
+
+const superheroSchema = new Schema(
+	{
+		id: { type: String, required: true},
+		name: { type: String, required: true },
+		powerstats: { type: Object, required: true },
+        appearance: { type: Object, required: true },
+		biography: { type: Object, required: true },
+		owner: {
+			type: Schema.Types.ObjectID,
+			ref: 'User',
+		}
+	},
+	{ timestamps: true }
+)
+
+const Superhero = model('Superhero', superheroSchema)
+
+/////////////////////////////////
+// Export our Model
+/////////////////////////////////
+module.exports = Superhero
